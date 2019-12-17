98e9e1f94243990f215fcedd45b27457b94dc824
bmarulanda <bmarulanda@DESKTOP-ABP47ER>
2019-12-17 16:58:26 -0500

    Datos del owner

--- Myvet.Web/Controllers/AccountController.cs
+++ Myvet.Web/Controllers/AccountController.cs
@@ -3,10 +3,56 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Myvet.Web.Helpers;
+using Myvet.Web.Helpers.Myvet.Web.Helpers;
+using Myvet.Web.Models;
 
 namespace Myvet.Web.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly IUserHelper _userHelper;
+
+        public AccountController(IUserHelper userHelper)
+        {
+            _userHelper = userHelper;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task <IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _userHelper.LoginAsync(model);
+                if (result.Succeeded)
+                {
+                    if (Request.Query.Keys.Contains("ReturnUrl"))
+                    {
+                        return Redirect(Request.Query["ReturnUrl"].First());
+                    }
+
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "User or password not valid.");
+
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Logout()
+        {
+            await _userHelper.LogoutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+
+        }
     }
-}
+
--- Myvet.Web/Controllers/ManagersController.cs
+++ Myvet.Web/Controllers/ManagersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,8 @@ using Myvet.Web.Data.Entities;
 
 namespace Myvet.Web.Controllers
 {
+    [Authorize(Roles = "Admin")]
+
     public class ManagersController : Controller
     {
         private readonly DataContext _context;
